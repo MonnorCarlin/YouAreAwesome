943c3f68af2e43fd5e5af5a2863928955baf604c
Connor Marlin <marlinc@bc.edu>
2019-06-04 21:23:57 -0400

    Changed to fit original name (should be first version)

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Laird Connor Stephen Marlin I, Exquire on 6/4.
+//  Copyright © 2019 Laird Connor Stephen Marlin I, Exquire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
